1df8726c48acbaedeeef63450acf1c94023c84aa
giftedbird <giftedbird@163.com>
2013-07-06 11:33:42 +0800

    peizhi

--- aituidao_server/settings.py
+++ aituidao_server/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/yuanzhe/Workspace/aituidao_server/sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/giftedbird/database/sqlite3/aituidao.db',
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
