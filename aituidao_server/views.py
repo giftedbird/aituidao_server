f4017e16e06456f10922a0d62f48d1183b636023
yuanzhe.guo <yuanzhe.guo@renren-inc.com>
2013-07-07 16:22:00 +0800

    demo book list

--- aituidao_server/views.py
+++ aituidao_server/views.py
@@ -88,9 +88,60 @@ def add_book_from_file(request, fileName):
 
 
 # internal function
-def book_list_internal(sortType, pageNo, count):
-    return ur'{"bookList":[{"author":"（日）麻耶雄嵩 ","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130701/137266854547779.jpg","id":50,"intro":"新本格鬼才麻耶雄嵩力作！这个家伙还能写出“中规中矩”的推理？！不信的话，就请阅读这部《贵族侦探》。","pushCount":4,"title":"贵族侦探"},{"author":"（日）麻耶雄嵩 ","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130701/137266854547779.jpg","id":50,"intro":"新本格鬼才麻耶雄嵩力作！这个家伙还能写出“中规中矩”的推理？！不信的话，就请阅读这部《贵族侦探》。","pushCount":4,"title":"贵族侦探"},{"author":"阿丫","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130617/137146100086799.jpg","id":59,"intro":"本书是一本培养着装风格，提升个人品味的书。资深时装造型师阿丫以平实的视角，将看似复杂高深的穿衣之道回归简单，诠释了第一夫人风格、中产LADY风格、“白骨精”风格、文艺青年风格、摇滚GIRL风格等，35种时尚单品，28种混搭技巧，教你聪明搭衣，精明购衣。同时记录了自己的生活感悟、着装心得及一些时装品牌的风格，并在不经意间透露了当下时装行业的小秘密。浸润其中你会慢慢成长为褪去时装，依然FASHION的时尚ICON。","pushCount":41,"title":"人群中,你就是那个“例外”"},{"author":"（日）麻耶雄嵩 ","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130701/137266854547779.jpg","id":50,"intro":"新本格鬼才麻耶雄嵩力作！这个家伙还能写出“中规中矩”的推理？！不信的话，就请阅读这部《贵族侦探》。","pushCount":4,"title":"贵族侦探"},{"author":"阿丫","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130617/137146100086799.jpg","id":59,"intro":"本书是一本培养着装风格，提升个人品味的书。资深时装造型师阿丫以平实的视角，将看似复杂高深的穿衣之道回归简单，诠释了第一夫人风格、中产LADY风格、“白骨精”风格、文艺青年风格、摇滚GIRL风格等，35种时尚单品，28种混搭技巧，教你聪明搭衣，精明购衣。同时记录了自己的生活感悟、着装心得及一些时装品牌的风格，并在不经意间透露了当下时装行业的小秘密。浸润其中你会慢慢成长为褪去时装，依然FASHION的时尚ICON。","pushCount":41,"title":"人群中,你就是那个“例外”"},{"author":"阿丫","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130617/137146100086799.jpg","id":59,"intro":"本书是一本培养着装风格，提升个人品味的书。资深时装造型师阿丫以平实的视角，将看似复杂高深的穿衣之道回归简单，诠释了第一夫人风格、中产LADY风格、“白骨精”风格、文艺青年风格、摇滚GIRL风格等，35种时尚单品，28种混搭技巧，教你聪明搭衣，精明购衣。同时记录了自己的生活感悟、着装心得及一些时装品牌的风格，并在不经意间透露了当下时装行业的小秘密。浸润其中你会慢慢成长为褪去时装，依然FASHION的时尚ICON。","pushCount":41,"title":"人群中,你就是那个“例外”"},{"author":"（日）麻耶雄嵩 ","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130701/137266854547779.jpg","id":50,"intro":"新本格鬼才麻耶雄嵩力作！这个家伙还能写出“中规中矩”的推理？！不信的话，就请阅读这部《贵族侦探》。","pushCount":4,"title":"贵族侦探"},{"author":"小泉吉宏","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130617/137146121452693.jpg","id":52,"intro":"★ 经典中的经典！畅销1800万册，横扫亚洲的“小幸福神书”首次登陆中国内地！","pushCount":23,"title":"佛陀与想太多的猪1"},{"author":"（日）麻耶雄嵩 ","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130701/137266854547779.jpg","id":50,"intro":"新本格鬼才麻耶雄嵩力作！这个家伙还能写出“中规中矩”的推理？！不信的话，就请阅读这部《贵族侦探》。","pushCount":4,"title":"贵族侦探"},{"author":"（日）麻耶雄嵩 ","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130701/137266854547779.jpg","id":50,"intro":"新本格鬼才麻耶雄嵩力作！这个家伙还能写出“中规中矩”的推理？！不信的话，就请阅读这部《贵族侦探》。","pushCount":4,"title":"贵族侦探"},{"author":"阿丫","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130617/137146100086799.jpg","id":59,"intro":"本书是一本培养着装风格，提升个人品味的书。资深时装造型师阿丫以平实的视角，将看似复杂高深的穿衣之道回归简单，诠释了第一夫人风格、中产LADY风格、“白骨精”风格、文艺青年风格、摇滚GIRL风格等，35种时尚单品，28种混搭技巧，教你聪明搭衣，精明购衣。同时记录了自己的生活感悟、着装心得及一些时装品牌的风格，并在不经意间透露了当下时装行业的小秘密。浸润其中你会慢慢成长为褪去时装，依然FASHION的时尚ICON。","pushCount":41,"title":"人群中,你就是那个“例外”"},{"author":"（日）麻耶雄嵩 ","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130701/137266854547779.jpg","id":50,"intro":"新本格鬼才麻耶雄嵩力作！这个家伙还能写出“中规中矩”的推理？！不信的话，就请阅读这部《贵族侦探》。","pushCount":4,"title":"贵族侦探"},{"author":"小泉吉宏","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130617/137146121452693.jpg","id":52,"intro":"★ 经典中的经典！畅销1800万册，横扫亚洲的“小幸福神书”首次登陆中国内地！","pushCount":23,"title":"佛陀与想太多的猪1"},{"author":"（日）麻耶雄嵩 ","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130701/137266854547779.jpg","id":50,"intro":"新本格鬼才麻耶雄嵩力作！这个家伙还能写出“中规中矩”的推理？！不信的话，就请阅读这部《贵族侦探》。","pushCount":4,"title":"贵族侦探"},{"author":"小泉吉宏","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130617/137146121452693.jpg","id":52,"intro":"★ 经典中的经典！畅销1800万册，横扫亚洲的“小幸福神书”首次登陆中国内地！","pushCount":23,"title":"佛陀与想太多的猪1"},{"author":"（日）麻耶雄嵩 ","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130701/137266854547779.jpg","id":50,"intro":"新本格鬼才麻耶雄嵩力作！这个家伙还能写出“中规中矩”的推理？！不信的话，就请阅读这部《贵族侦探》。","pushCount":4,"title":"贵族侦探"},{"author":"小泉吉宏","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130617/137146121452693.jpg","id":52,"intro":"★ 经典中的经典！畅销1800万册，横扫亚洲的“小幸福神书”首次登陆中国内地！","pushCount":23,"title":"佛陀与想太多的猪1"},{"author":"小泉吉宏","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130617/137146121452693.jpg","id":52,"intro":"★ 经典中的经典！畅销1800万册，横扫亚洲的“小幸福神书”首次登陆中国内地！","pushCount":23,"title":"佛陀与想太多的猪1"},{"author":"阿丫","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130617/137146100086799.jpg","id":59,"intro":"本书是一本培养着装风格，提升个人品味的书。资深时装造型师阿丫以平实的视角，将看似复杂高深的穿衣之道回归简单，诠释了第一夫人风格、中产LADY风格、“白骨精”风格、文艺青年风格、摇滚GIRL风格等，35种时尚单品，28种混搭技巧，教你聪明搭衣，精明购衣。同时记录了自己的生活感悟、着装心得及一些时装品牌的风格，并在不经意间透露了当下时装行业的小秘密。浸润其中你会慢慢成长为褪去时装，依然FASHION的时尚ICON。","pushCount":41,"title":"人群中,你就是那个“例外”"},{"author":"小泉吉宏","coverUrl":"http://d31i1rfrna7v3n.cloudfront.net/image/20130617/137146121452693.jpg","id":52,"intro":"★ 经典中的经典！畅销1800万册，横扫亚洲的“小幸福神书”首次登陆中国内地！","pushCount":23,"title":"佛陀与想太多的猪1"}],"nextPageNum":1,"status":1}'
+class BookJson:
+    pass;
 
+class BookListJson:
+    pass;
+
+SORT_TYPE_TIME = 1
+SORT_TYPE_HOT = 2
+
+def book_list_internal(sortType, pageNo, count):
+    if sortType == SORT_TYPE_TIME:
+        #  需要重新写
+        books = Book.objects.all();
+        
+        bookJsonList = [];
+        for book in books:
+            bookItem = BookJson()
+            bookItem.id = book.id
+            bookItem.title = book.title
+            bookItem.author = book.author
+            bookItem.intro = book.intro
+            bookItem.coverUrl = book.cover
+            bookItem.pushCount = book.pushCount
+            
+            bookJsonList.append(bookItem)
+        
+        bookListJson = BookListJson()
+        bookListJson.bookList = bookJsonList
+        bookListJson.nextPageNum = 1
+        
+        return json.dump(bookListJson)
+    elif sortType == SORT_TYPE_HOT:
+        #  需要重新写
+        books = Book.objects.all();
+        
+        bookJsonList = [];
+        for book in books:
+            bookItem = BookJson()
+            bookItem.id = book.id
+            bookItem.title = book.title
+            bookItem.author = book.author
+            bookItem.intro = book.intro
+            bookItem.coverUrl = book.cover
+            bookItem.pushCount = book.pushCount
+            
+            bookJsonList.append(bookItem)
+        
+        bookListJson = BookListJson()
+        bookListJson.bookList = bookJsonList
+        bookListJson.nextPageNum = 1
+        
+        return json.dump(bookListJson)
+    else:
+        return None
 
 def push_book_internal(bookId, addr):
     return ur'{"status":1}'
